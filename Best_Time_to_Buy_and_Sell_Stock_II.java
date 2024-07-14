87a1a0616211162c39e7d4d90cb5f18df9ac3b71
manikerisaurabh <manikerisaurabh@gmail.com>
2024-07-14 23:22:16 +0530

    Best Time to Buy and Sell Stock II :+1:

---
+++ Best_Time_to_Buy_and_Sell_Stock_II.java
@@ -0,0 +1,15 @@
+public class Best_Time_to_Buy_and_Sell_Stock_II {
+    class Solution {
+        public int maxProfit(int[] prices) {
+            int max = 0;
+            int start = prices[0];
+            int len = prices.length;
+            for (int i = 1; i < len; i++) {
+                if (start < prices[i])
+                    max += prices[i] - start;
+                start = prices[i];
+            }
+            return max;
+        }
+    }
+}
