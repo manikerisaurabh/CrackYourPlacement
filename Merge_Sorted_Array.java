35dfd6722a7c7eff93f2d65bf3c4fb7a21358f22
manikerisaurabh <manikerisaurabh@gmail.com>
2024-07-15 00:36:28 +0530

    Merges Sortes Array :+1:

---
+++ Merge_Sorted_Array.java
@@ -0,0 +1,13 @@
+public class Merge_Sorted_Array {
+    class Solution {
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
+            int a = m - 1, b = n - 1, c = nums1.length - 1;
+            while (b >= 0) {
+                if (a >= 0 && (nums1[a] > nums2[b]))
+                    nums1[c--] = nums1[a--];
+                else
+                    nums1[c--] = nums2[b--];
+            }
+        }
+    }
+}
