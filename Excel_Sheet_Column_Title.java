6c4a97ea40dae86eb446986ce6f6a2e42e345bcf
manikerisaurabh <manikerisaurabh@gmail.com>
2024-07-15 23:25:37 +0530

    Excel Sheet Column Title :+1:

---
+++ Excel_Sheet_Column_Title.java
@@ -0,0 +1,16 @@
+public class Excel_Sheet_Column_Title {
+    class Solution {
+        public String convertToTitle(int columnNumber) {
+            StringBuilder ans = new StringBuilder();
+
+            while (columnNumber > 0) {
+                columnNumber--;
+
+                ans.append((char) ((columnNumber) % 26 + 'A'));
+                columnNumber = (columnNumber) / 26;
+            }
+
+            return ans.reverse().toString();
+        }
+    }
+}
