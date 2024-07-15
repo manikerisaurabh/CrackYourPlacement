c5f84cea0317e32260d3a5a85c1b39450ea30f00
manikerisaurabh <manikerisaurabh@gmail.com>
2024-07-15 18:23:27 +0530

    Generate Parentheses :+1:

---
+++ Generate_Parentheses.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generate_Parentheses {
+    class Solution {
+        public static List<String> generateParenthesisAns(String p, int open, int close, ArrayList<String> list) {
+            if (open == 0 && close == 0) {
+                list.add(p);
+                return list;
+            }
+
+            if (open != 0) {
+                generateParenthesisAns(p + "(", open - 1, close, list);
+            }
+            if (close > open) {
+                generateParenthesisAns(p + ")", open, close - 1, list);
+            }
+
+            return list;
+        }
+
+        public List<String> generateParenthesis(int n) {
+            return generateParenthesisAns("", n, n, new ArrayList<>());
+        }
+    }
+}
