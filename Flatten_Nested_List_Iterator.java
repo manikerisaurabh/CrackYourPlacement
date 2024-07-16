e9db6d6228fa04ad0fb58075216942c4f6a3310e
manikerisaurabh <manikerisaurabh@gmail.com>
2024-07-17 00:21:02 +0530

    Flatten Nested List Iterator :+1:

---
+++ Flatten_Nested_List_Iterator.java
@@ -0,0 +1,35 @@
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Stack;
+
+public class Flatten_Nested_List_Iterator {
+    public class NestedIterator implements Iterator<Integer> {
+
+        public NestedIterator(List<NestedInteger> nestedList) {
+            lists = new Stack<>();
+            lists.push(nestedList.listIterator());
+        }
+
+        public Integer next() {
+            hasNext();
+            return lists.peek().next().getInteger();
+        }
+
+        public boolean hasNext() {
+            while (!lists.empty()) {
+                if (!lists.peek().hasNext()) {
+                    lists.pop();
+                } else {
+                    NestedInteger x = lists.peek().next();
+                    if (x.isInteger())
+                        return lists.peek().previous() == x;
+                    lists.push(x.getList().listIterator());
+                }
+            }
+            return false;
+        }
+
+        private Stack<ListIterator<NestedInteger>> lists;
+    }
+}
