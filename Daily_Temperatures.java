e9fe9bb013afd8085fe46b46889093d5bcd77446
manikerisaurabh <manikerisaurabh@gmail.com>
2024-07-17 00:10:41 +0530

    Dail Temprature :+1:

---
+++ Daily_Temperatures.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+
+public class Daily_Temperatures {
+    class Solution {
+        public int[] dailyTemperatures(int[] temps) {
+            int[] results = new int[temps.length];
+            Stack<Integer> stack = new Stack<>();
+            for (int i = 0; i < temps.length; i++) {
+                while (!stack.isEmpty() && temps[stack.peek()] < temps[i]) {
+                    results[stack.peek()] = i - stack.pop();
+                }
+                stack.push(i);
+            }
+
+            return results;
+        }
+    }
+}
