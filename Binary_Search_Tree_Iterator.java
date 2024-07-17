a842615d62b19a9a80fdce1e98e3f90ee47c0710
manikerisaurabh <manikerisaurabh@gmail.com>
2024-07-17 23:51:19 +0530

    Binary Search Tree Iterator :+1:

---
+++ Binary_Search_Tree_Iterator.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+import javax.swing.tree.TreeNode;
+
+public class Binary_Search_Tree_Iterator {
+
+    class BSTIterator {
+        Stack<TreeNode> s = new Stack<>();
+
+        public BSTIterator(TreeNode root) {
+            help(root);
+        }
+
+        public int next() {
+            TreeNode h = s.pop();
+            help(h.right);
+            return h.val;
+        }
+
+        public boolean hasNext() {
+            if (s.isEmpty())
+                return false;
+            return true;
+        }
+
+        void help(TreeNode r) {
+            if (r == null)
+                return;
+            s.push(r);
+            help(r.left);
+        }
+    }
+
+}
