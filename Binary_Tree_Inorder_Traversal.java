e2e57f731181bc711a0176ecaea5bc2a03588010
manikerisaurabh <manikerisaurabh@gmail.com>
2024-07-17 23:48:46 +0530

    Binary Tree Inorder Traversal :+1:

---
+++ Binary_Tree_Inorder_Traversal.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
+
+public class Binary_Tree_Inorder_Traversal {
+
+    class Solution {
+        public List<Integer> inorderTraversal(TreeNode root) {
+            List<Integer> list = new ArrayList<>();
+            inorderTraversal(root, list);
+            return list;
+        }
+
+        void inorderTraversal(TreeNode node, List<Integer> list) {
+            if (node == null) {
+                return;
+            }
+
+            inorderTraversal(node.left, list); // Traverse the left subtree
+            list.add(node.val); // Visit the root node
+            inorderTraversal(node.right, list); // Traverse the right subtree
+        }
+    }
+}
