afc498e2be93babd6aaac1b036e66791cd1940fe
manikerisaurabh <manikerisaurabh@gmail.com>
2024-07-20 23:32:17 +0530

    Subsets II :+1:

---
+++ Subsets_II.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Subsets_II {
+    class Solution {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        public List<List<Integer>> subsetsWithDup(int[] nums) {
+            Arrays.sort(nums);
+            findAns(0, nums, new ArrayList<>());
+            return ans;
+        }
+
+        public void findAns(int n, int[] nums, List<Integer> al) {
+            ans.add(new ArrayList<>(al));
+            for (int i = n; i < nums.length; i++) {
+                if (i > n && nums[i] == nums[i - 1]) {
+                    continue;
+                }
+                al.add(nums[i]);
+                findAns(i + 1, nums, al);
+                al.remove(al.size() - 1);
+
+            }
+
+        }
+    }
+}
