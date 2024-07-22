cd0c08982727addefb3217301193b824e313eb11
manikerisaurabh <manikerisaurabh@gmail.com>
2024-07-22 20:08:46 +0530

    Minimum Deletions to Make Character Frequencies Unique :+1:

---
+++ Minimum_Deletions_to_Make_Character_Frequencies_Unique.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class Minimum_Deletions_to_Make_Character_Frequencies_Unique {
+    class Solution {
+        public int minDeletions(String s) {
+            Map<Character, Integer> map = new HashMap<>();
+            System.out.println(map);
+
+            for (int i = 0; i < s.length(); i++)
+                map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+
+            int result = 0;
+            HashSet<Integer> set = new HashSet<>();
+
+            for (Map.Entry<Character, Integer> mapElement : map.entrySet()) {
+                int currentVal = mapElement.getValue();
+                if (!set.contains(currentVal))
+                    set.add(currentVal);
+                else {
+                    while (set.contains(currentVal) && currentVal > 0) {
+                        currentVal--;
+                        result++;
+                    }
+                    if (currentVal > 0)
+                        set.add(currentVal);
+                }
+            }
+            return result;
+        }
+    }
+}
